1d16c91184f2ee3c62334b4c2e753420d461742b
Pa7ry <patriciadesantiagogallego@gmail.com>
2017-12-20 13:56:29 +0100

    php
    http://evelb.es/blog/css3-menu-desplegable-con-hover/

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <title>BananaTube</title>
+    
+</head>
+<body> 
+    <?php include 'includes/header.html'?>
+    <?php include 'includes/menu.html'?>
+    <?php include 'index_contenido.html'?>
+    <?php include 'includes/footer.html'?>
+</body>
+</html>
