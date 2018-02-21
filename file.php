e4d9abbc041cb59ea4d6f2721217b2dee168ab07
NereaGene <neramirez25@gmail.com>
2018-02-21 12:16:49 +0100

    intentando subida de videos

---
+++ file.php
@@ -0,0 +1,14 @@
+<?php
+$baseUrl="http://localhost/html5/bananatubehtml/datas";
+if (isset($_FILES['upload_file'])) {
+    if(move_uploaded_file($_FILES['upload_file']['tmp_name'], getcwd().DIRECTORY_SEPARATOR."datas/" . $_FILES['upload_file']['name'])){
+        
+        echo $baseUrl.$_FILES['upload_file']['name'];
+    } else {
+        echo "Error:".$_FILES['upload_file']['name']. " KO";
+    }
+    exit;
+} else {
+    echo "No files uploaded ...";
+}
+?>
